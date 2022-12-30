46cbbb8a4c91780942b2c945cfe4982b43abbfdf
moschidisg <mosxidismosxidis@gmail.com>
2022-12-30 14:56:33 +0200

    add routes folder

---
+++ src/routes/LogIn.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+// import components
+
+const LogIn = (props) => {
+    const user = props.user;
+    const setUser = props.setUser;
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    if (user) {
+        console.log(user);
+        console.log('logged in');
+    } else {
+        console.log('not logged in');
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await fetch('http://localhost:5000/api/auth/log-in', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        });
+        const data = await response.json();
+        console.log(data);
+        // if (data.user) {
+        //     localStorage.setItem('user', JSON.stringify(data.user));
+        // }
+    };
+
+    return (
+        <div className="body">
+            <form onSubmit={handleSubmit} action="" method="POST">
+                <label htmlFor="email">Email</label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    className="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <button className="submit">Log In</button>
+            </form>
+        </div>
+    );
+};
+
+export default LogIn;
