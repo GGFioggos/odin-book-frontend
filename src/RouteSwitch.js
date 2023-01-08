16fe96f653c66fd3cc80c01dbe33c51d87ccc66c
moschidisg <mosxidismosxidis@gmail.com>
2023-01-08 21:10:37 +0200

    add new routes

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,13 +1,21 @@
 import { Route, HashRouter, Routes } from 'react-router-dom';
-import App from './App';
-import Shop from './routes/Shop';
+import React, { useState, useEffect } from 'react';
+// import App from './App';
+import LogIn from './routes/LogIn';
 
 const RouteSwitch = () => {
+    const [user, setUser] = useState(null);
+
     return (
         <HashRouter basename="/">
             <Routes>
+                <Route
+                    path="/"
+                    element={<LogIn user={user} setUser={setUser} />}
+                />
                 <Route path="/home" /*element={<App />} */ />
-                <Route path="/log-in" /* element={<LogIn />} */ />
+                <Route path="/profile" /* element={<Profile />} */ />
+                <Route path="/user" /* element={<User />} */ />
             </Routes>
         </HashRouter>
     );
