3b2b2912c51f4b24111d6c8da609e513646a2ae2
moschidisg <mosxidismosxidis@gmail.com>
2023-02-17 18:16:40 +0200

    fix authentication

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect, useContext } from 'react';
+import Header from '../components/Header';
 import { UserContext } from '../UserContext';
 
 function App() {
     const { userInfo } = useContext(UserContext);
-    console.log(userInfo);
-    return <div>Hello {userInfo && <div>{userInfo.firstName}</div>}</div>;
+
+    return (
+        <div>
+            <Header />
+            <div>
+                {userInfo == null && (
+                    <div>Hello! Please log in or register to gain access</div>
+                )}
+                {userInfo != null && <div>Hello {userInfo.lastName}</div>}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/routes/LogIn.js
+++ src/routes/LogIn.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import Header from '../components/Header';
 import { UserContext } from '../UserContext';
 // import components
 
@@ -7,7 +8,7 @@ const LogIn = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
-    const { setUserInfo } = useContext(UserContext);
+    const { setUserInfo, userInfo } = useContext(UserContext);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -25,8 +26,10 @@ const LogIn = (props) => {
         });
         if (response.ok) {
             const data = await response.json();
-            setUserInfo(data.user);
-            setRedirect(true);
+            if (data.user) {
+                setUserInfo(data.user);
+                setRedirect(true);
+            }
         } else {
             alert('Wrong credentials');
         }
@@ -35,29 +38,37 @@ const LogIn = (props) => {
     if (redirect) {
         return <Navigate to={'/'} />;
     }
+
+    if (userInfo) {
+        return <Navigate to={'/'} />;
+    }
+
     return (
-        <div className="body">
-            <form onSubmit={handleSubmit} action="" method="POST">
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    className="email"
-                    placeholder="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                ></input>
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    className="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                ></input>
-                <button className="submit">Log In</button>
-            </form>
+        <div>
+            <Header />
+            <div className="body">
+                <form onSubmit={handleSubmit} action="" method="POST">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        className="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></input>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        className="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                    <button className="submit">Log In</button>
+                </form>
+            </div>
         </div>
     );
 };
