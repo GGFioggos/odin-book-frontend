92ec592b2f5242a05bf2d02f9f838745f3b1707d
moschidisg <mosxidismosxidis@gmail.com>
2023-02-21 19:34:57 +0200

    create divider component

---
+++ src/components/Divider.js
@@ -0,0 +1,12 @@
+const Divider = () => (
+    <div
+        style={{
+            marginTop: '5px',
+            width: '100%',
+            height: '1px',
+            borderBottom: '1px solid #686969',
+        }}
+    />
+);
+
+export default Divider;
