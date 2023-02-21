6d1c923687295d680c0d7421bb2fe2647f8a3a0b
moschidisg <mosxidismosxidis@gmail.com>
2023-02-21 19:36:23 +0200

    Add post information and formatting

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,7 @@
+import Divider from './Divider';
+
 const Post = (props) => {
-    const { author, content, timestamp, comments, likes } = props.post;
+    const { author, content, comments, likes } = props.post;
 
     return (
         <div className="post">
@@ -9,18 +11,28 @@ const Post = (props) => {
                     src={author.profilePictureUrl}
                     alt="profile"
                 ></img>
-                <div className="firstName">{author.firstName}</div>
+                <div>
+                    <div className="firstName">{author.firstName}</div>
+                    <div className="date">{props.post.time_diff}</div>
+                </div>
             </div>
             <div className="content">{content}</div>
             <div className="impressions">
-                <div className="likes">{likes.length} Likes</div>
-                <div className="commentsCount">{comments.length} Comments</div>
+                <div className="likes">
+                    {likes.length} {likes.length === 1 ? 'Like' : 'Likes'}
+                </div>
+                <div className="commentsCount">
+                    {comments.length}{' '}
+                    {comments.length === 1 ? 'Comment' : 'Comments'}
+                </div>
             </div>
+            <Divider />
             <ul className="actions">
                 <li>Like</li>
                 <li>Comment</li>
                 <li>Share</li>
             </ul>
+            {comments.length > 0 && <Divider />}
             <div className="comments">
                 {comments.map((comment, i) => {
                     return <div key={i}>{comment.content}</div>;
