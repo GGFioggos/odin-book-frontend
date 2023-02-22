dd7fc25888625ecfa0eb541d4be8edfdaf89410a
moschidisg <mosxidismosxidis@gmail.com>
2023-02-22 23:14:44 +0200

    add like function

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,41 @@
 import Divider from './Divider';
 import Comment from './Comment';
+import { UserContext } from '../UserContext';
+import { useContext, useEffect, useState } from 'react';
 const Post = (props) => {
     const { author, content, comments, likes } = props.post;
+    const { userInfo } = useContext(UserContext);
+
+    const [postLikes, setPostLikes] = useState(likes.length);
+    const [liked, setLiked] = useState(false);
+
+    useEffect(() => {
+        if (likes.includes(userInfo._id)) {
+            setLiked(true);
+        }
+    }, []);
+
+    function like() {
+        setLiked(!liked);
+        if (liked) {
+            setPostLikes(postLikes - 1);
+        } else {
+            setPostLikes(postLikes + 1);
+        }
+        fetch(
+            `http://localhost:5000/api/post/${props.post._id}/${
+                liked ? 'unlike' : 'like'
+            }`,
+            {
+                method: 'POST',
+                credentials: 'include',
+            }
+        ).then((response) => {
+            response.json().then((data) => {
+                console.log(data);
+            });
+        });
+    }
 
     return (
         <div className="post">
@@ -19,7 +53,7 @@ const Post = (props) => {
             <div className="content">{content}</div>
             <div className="impressions">
                 <div className="likes">
-                    {likes.length} {likes.length === 1 ? 'Like' : 'Likes'}
+                    {postLikes} {postLikes === 1 ? 'Like' : 'Likes'}
                 </div>
                 <div className="commentsCount">
                     {comments.length}{' '}
@@ -28,9 +62,16 @@ const Post = (props) => {
             </div>
             <Divider />
             <ul className="actions">
-                <li>Like</li>
-                <li>Comment</li>
-                <li>Share</li>
+                <button
+                    onClick={like}
+                    style={{
+                        color: liked ? '#2d86ff' : 'white',
+                    }}
+                >
+                    {liked ? 'Liked' : 'Like'}
+                </button>
+                <button>Comment</button>
+                <button>Share</button>
             </ul>
             {comments.length > 0 && <Divider />}
             <div className="comments">
